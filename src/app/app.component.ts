069e11ad8e51a53088adefff46b1a361cc89e884
shiv <6rashmijoshi@gmail.com>
2021-07-11 16:48:46 +0530

    forth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { repos } from './Models/repos';
 import { GitHubService } from './services/github.service';
+
  @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GitHubService } from './services/github.service';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,NgModule,FormsModule 
+    BrowserModule,NgModule,FormsModule,HttpClientModule
   ],
   providers: [GitHubService],
   bootstrap: [AppComponent]
